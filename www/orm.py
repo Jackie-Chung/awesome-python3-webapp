75ad0bfec69a1ded73ad49a1ad7c6b8788a22f2d
Jackie <jackiechung927@gmail.com>
2015-12-23 21:57:21 +0800

    follow the demo

--- www/orm.py
+++ www/orm.py
@@ -3,6 +3,13 @@
 
 __author__ = 'Jackie Chan'
 
+import asyncio, logging
+
+import aiomysql
+
+def log(sql, args=()):
+	logging.info('SQL: %s' % sql)
+
 #创建一个全局的连接池，每个HTTP请求都可以从连接池中直接获取数据库连接
 @asyncio.coroutine
 def creat_pool(loop,**kw):
@@ -49,50 +56,11 @@ def excute(sql, args):
 			raise
 		return affected
 
-
-from orm import Model, StringField, IntegerField
-
-class User(Model):
-	__table__ = 'users'
-
-	id = IntegerField(primary_key = True)
-	name = StringField()
-
-#创建实例:
-user = User(id = 123, name = 'Michael')
-#存入数据库：
-user.insert()
-#查询所有User对象：
-users = user.findAll()
-
-
-#定义的所有ORM映射的基类Model：
-class Model(dict, metaclass = ModelMetaclass):
-
-	def __init__(self, **kw):
-		super(Model, self).__init__(**kw)
-
-	def __getattr__(self, key):
-		try:
-			return self[key]
-		except KeyError:
-			raise AttributeError(r"'Model' object has no attribute '%s'" % key)
-
-	def __setattr__(self, key, value):
-		self[key] = value
-
-	def getValue(self, key):
-		return getattr(self, key, None)
-
-	def  getValueOrDefalut(self, key):
-		value = getattr(self, key, None)
-		if value is None:
-			field = self.__mappings__[key]
-			if field.default is not None:
-				value = field.default() if callable(field.default) else field.default
-				logging.debug('using default value for %s: %s' % (key, str(value)))
-				setattr(self, key, value)
-		return value
+def creat_args_string(num):
+	L = []
+	for n in range(num):
+		L.append('?')
+	return ', '.join(L)
 
 #定义Field和各种Field子类：
 class Field(object):
@@ -112,6 +80,25 @@ class StringField(Field):
 	def __init__(self, name = None, primary_key = False, default = None, ddl = 'varchar(100)'):
 		super().__init__(name, ddl, primary_key, default)
 
+class BooleanField(Field):
+
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
 
 class Modelmetaclass(type):
 
@@ -153,3 +140,105 @@ class Modelmetaclass(type):
 		attrs['__update__'] = 'update `%s` set %s where `%s` = ?' % (tableName, ', '.join(map(lambda f: '`%s` = ?' % (mappings.get(f).name of f), fields)), primaryKey)
 		attrs['__delete__'] = 'delete from `%s` where `%s` = ?' % (tableName, primaryKey)
 		return type.__new__(cls, name, bases, attrs)
+
+#定义的所有ORM映射的基类Model：
+class Model(dict, metaclass = ModelMetaclass):
+
+	def __init__(self, **kw):
+		super(Model, self).__init__(**kw)
+
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+	def __setattr__(self, key, value):
+		self[key] = value
+
+	def getValue(self, key):
+		return getattr(self, key, None)
+
+	def  getValueOrDefalut(self, key):
+		value = getattr(self, key, None)
+		if value is None:
+			field = self.__mappings__[key]
+			if field.default is not None:
+				value = field.default() if callable(field.default) else field.default
+				logging.debug('using default value for %s: %s' % (key, str(value)))
+				setattr(self, key, value)
+		return value
+
+	#网Model类添加class方法，就可以让所有子类调用class方法：
+	@classmethod
+    @asyncio.coroutine
+    def findAll(cls, where=None, args=None, **kw):
+        ' find objects by where clause. '
+        sql = [cls.__select__]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+            args = []
+        orderBy = kw.get('orderBy', None)
+        if orderBy:
+            sql.append('order by')
+            sql.append(orderBy)
+        limit = kw.get('limit', None)
+        if limit is not None:
+            sql.append('limit')
+            if isinstance(limit, int):
+                sql.append('?')
+                args.append(limit)
+            elif isinstance(limit, tuple) and len(limit) == 2:
+                sql.append('?, ?')
+                args.extend(limit)
+            else:
+                raise ValueError('Invalid limit value: %s' % str(limit))
+        rs = yield from select(' '.join(sql), args)
+        return [cls(**r) for r in rs]
+
+    @classmethod
+    @asyncio.coroutine
+    def findNumber(cls, selectField, where=None, args=None):
+        ' find number by select and where. '
+        sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        rs = yield from select(' '.join(sql), args, 1)
+        if len(rs) == 0:
+            return None
+        return rs[0]['_num_']
+
+    @classmethod
+    @asyncio.coroutine
+    def find(cls, pk):
+        ' find object by primary key. '
+        rs = yield from select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
+
+    @asyncio.coroutine
+    def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = yield from execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn('failed to insert record: affected rows: %s' % rows)
+
+    @asyncio.coroutine
+    def update(self):
+        args = list(map(self.getValue, self.__fields__))
+        args.append(self.getValue(self.__primary_key__))
+        rows = yield from execute(self.__update__, args)
+        if rows != 1:
+            logging.warn('failed to update by primary key: affected rows: %s' % rows)
+
+    @asyncio.coroutine
+    def remove(self):
+        args = [self.getValue(self.__primary_key__)]
+        rows = yield from execute(self.__delete__, args)
+        if rows != 1:
+            logging.warn('failed to remove by primary key: affected rows: %s' % rows)
